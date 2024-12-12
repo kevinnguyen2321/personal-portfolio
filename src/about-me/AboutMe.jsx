b498bb22fd16610a03919f668fff6dcad96b4b77
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2024-12-11 21:57:36 -0600

    Add responsive design to project

--- src/about-me/AboutMe.jsx
+++ src/about-me/AboutMe.jsx
@@ -17,12 +17,12 @@ export const AboutMe = () => {
             which ultimately led me to web development. With a background in
             psychology, including Applied Behavioral Analysis (ABA) therapy,
             psychiatric hospital work, and case management, I’ve developed
-            strong analytical and problem-solving skills.I began self-learning web
-            development in 2023 and quickly fell in love with the entire process of
-            building and creating through code. I thrive on collaborating with
-            teams to turn ideas into reality and bring a unique perspective by
-            blending my passion for technology with my experience in
-            understanding human behavior.
+            strong analytical and problem-solving skills.I began self-learning
+            web development in 2023 and quickly fell in love with the entire
+            process of building and creating through code. I thrive on
+            collaborating with teams to turn ideas into reality and bring a
+            unique perspective by blending my passion for technology with my
+            experience in understanding human behavior.
           </p>
         </div>
       </div>
